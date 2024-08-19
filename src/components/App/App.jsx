d8a49b7e15c88b0555e3c965bb9b3379f6b7e2b6
KoptevAnton <koptev110593@gmail.com>
2024-08-19 22:40:13 +0300

    Init project. Add structure, dependencies, settings, default styles.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  );
+}
